d40c08f513373cf6aa8b9405c7bdc1114818f219
Olga MaciaszekSharma <olga.maciaszek@gmail.com>
2023-04-14 18:02:38 +0200

    Add initial Kafka setup and initial communication.

--- barista/src/main/java/io/spring/barcelona/coffee/barista/BaristaApplication.java
+++ barista/src/main/java/io/spring/barcelona/coffee/barista/BaristaApplication.java
@@ -1,13 +1,39 @@
 package io.spring.barcelona.coffee.barista;
 
+import io.spring.barcelona.coffee.barista.orders.Order;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.kafka.clients.admin.NewTopic;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.converter.RecordMessageConverter;
+import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 
 @SpringBootApplication
 public class BaristaApplication {
 
+	private static final Log LOG = LogFactory.getLog(BaristaApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BaristaApplication.class, args);
+	}
+
+	@Bean
+	NewTopic orders() {
+		return new NewTopic("orders", 1, (short) 1);
+	}
+
+	@Bean
+	public RecordMessageConverter converter() {
+		return new StringJsonMessageConverter();
+	}
+
+	@KafkaListener(id = "barista", topics = "orders")
+	public void listen(Order order) {
+		LOG.info(order);
 	}
 
 }
--- waiter/src/main/java/io/spring/barcelona/coffee/waiter/WaiterApplication.java
+++ waiter/src/main/java/io/spring/barcelona/coffee/waiter/WaiterApplication.java
@@ -1,13 +1,17 @@
 package io.spring.barcelona.coffee.waiter;
 
 import io.spring.barcelona.coffee.waiter.orders.Order;
+import org.apache.kafka.clients.admin.NewTopic;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.converter.RecordMessageConverter;
+import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +25,16 @@ public class WaiterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WaiterApplication.class, args);
+	}
+
+	@Bean
+	NewTopic orders() {
+		return new NewTopic("orders", 1, (short) 1);
+	}
+
+	@Bean
+	public RecordMessageConverter converter() {
+		return new StringJsonMessageConverter();
 	}
 
 	@PostMapping("/order/{name}/{count}")
