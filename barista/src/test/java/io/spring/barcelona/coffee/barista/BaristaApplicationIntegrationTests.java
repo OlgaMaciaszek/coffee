88176b3223802d3f24ada697d17faab22113c64e
Olga MaciaszekSharma <olga.maciaszek@gmail.com>
2023-05-17 16:33:58 +0200

    Remove unnecessary imports.

--- barista/src/test/java/io/spring/barcelona/coffee/barista/BaristaApplicationIntegrationTests.java
+++ barista/src/test/java/io/spring/barcelona/coffee/barista/BaristaApplicationIntegrationTests.java
@@ -1,23 +1,10 @@
 package io.spring.barcelona.coffee.barista;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.TimeUnit;
-
-import javax.annotation.Nullable;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.testcontainers.containers.KafkaContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.OutputCaptureExtension;
@@ -40,8 +27,17 @@ import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -55,73 +51,73 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Testcontainers
 class BaristaApplicationIntegrationTests {
 
-	private static final Log LOG = LogFactory.getLog(BaristaApplicationIntegrationTests.class);
+    private static final Log LOG = LogFactory.getLog(BaristaApplicationIntegrationTests.class);
 
-	BlockingQueue<Message<?>> broker = new ArrayBlockingQueue<>(1);
+    BlockingQueue<Message<?>> broker = new ArrayBlockingQueue<>(1);
 
-	@Container
-	@ServiceConnection
-	static KafkaContainer kafkaForContracts = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka"));
+    @Container
+    @ServiceConnection
+    static KafkaContainer kafkaForContracts = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka"));
 
-	@Autowired
-	StubTrigger trigger;
+    @Autowired
+    StubTrigger trigger;
 
-	@Test
-	void shouldProcessOrder() throws InterruptedException {
-		trigger.trigger("order");
+    @Test
+    void shouldProcessOrder() throws InterruptedException {
+        trigger.trigger("order");
 
-		Message<?> servingMessage = broker.poll(10, TimeUnit.SECONDS);
+        Message<?> servingMessage = broker.poll(10, TimeUnit.SECONDS);
 
-		assertThat(servingMessage).isNotNull();
-		assertThat(servingMessage.getPayload().toString()).contains("""
-				"coffee":{"name":"Latte","coffeeContent":60,"steamedMilkContent":180,"milkFoamContent":5""");
-	}
+        assertThat(servingMessage).isNotNull();
+        assertThat(servingMessage.getPayload().toString()).contains("""
+                "coffee":{"name":"Latte","coffeeContent":60,"steamedMilkContent":180,"milkFoamContent":5""");
+    }
 
 
-	@KafkaListener(id = "baristaServings", topics = "servings")
-	public void listen(ConsumerRecord payload, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
-		LOG.info("Got a message from a topic [" + topic + "]");
-		Map<String, Object> headers = new HashMap<>();
-		new DefaultKafkaHeaderMapper().toHeaders(payload.headers(), headers);
-		broker.add(MessageBuilder.createMessage(payload.value(), new MessageHeaders(headers)));
-	}
+    @KafkaListener(id = "baristaServings", topics = "servings")
+    public void listen(ConsumerRecord payload, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        LOG.info("Got a message from a topic [" + topic + "]");
+        Map<String, Object> headers = new HashMap<>();
+        new DefaultKafkaHeaderMapper().toHeaders(payload.headers(), headers);
+        broker.add(MessageBuilder.createMessage(payload.value(), new MessageHeaders(headers)));
+    }
 
 }
 
 @Configuration
 class TestConfig {
 
-	@Bean
-	MessageVerifierSender<Message<?>> standaloneMessageVerifier(KafkaTemplate kafkaTemplate) {
-		return new MessageVerifierSender<>() {
+    @Bean
+    MessageVerifierSender<Message<?>> standaloneMessageVerifier(KafkaTemplate kafkaTemplate) {
+        return new MessageVerifierSender<>() {
 
-			@Override
-			public void send(Message<?> message, String destination, @Nullable YamlContract contract) {
-			}
+            @Override
+            public void send(Message<?> message, String destination, @Nullable YamlContract contract) {
+            }
 
-			@Override
-			public <T> void send(T payload, Map<String, Object> headers, String destination, @Nullable YamlContract contract) {
-				Map<String, Object> newHeaders = headers != null ? new HashMap<>(headers) : new HashMap<>();
-				newHeaders.put(KafkaHeaders.TOPIC, destination);
-				kafkaTemplate.send(MessageBuilder.createMessage(payload, new MessageHeaders(newHeaders)));
-			}
-		};
-	}
+            @Override
+            public <T> void send(T payload, Map<String, Object> headers, String destination, @Nullable YamlContract contract) {
+                Map<String, Object> newHeaders = headers != null ? new HashMap<>(headers) : new HashMap<>();
+                newHeaders.put(KafkaHeaders.TOPIC, destination);
+                kafkaTemplate.send(MessageBuilder.createMessage(payload, new MessageHeaders(newHeaders)));
+            }
+        };
+    }
 
-	@Bean
-	@Primary
-	JsonMessageConverter noopMessageConverter() {
-		return new NoopJsonMessageConverter();
-	}
+    @Bean
+    @Primary
+    JsonMessageConverter noopMessageConverter() {
+        return new NoopJsonMessageConverter();
+    }
 }
 
 class NoopJsonMessageConverter extends JsonMessageConverter {
 
-	NoopJsonMessageConverter() {
-	}
+    NoopJsonMessageConverter() {
+    }
 
-	@Override
-	protected Object convertPayload(Message<?> message) {
-		return message.getPayload();
-	}
+    @Override
+    protected Object convertPayload(Message<?> message) {
+        return message.getPayload();
+    }
 }
--- barista/src/test/java/io/spring/barcelona/coffee/barista/BaseTestClass.java
+++ barista/src/test/java/io/spring/barcelona/coffee/barista/BaseTestClass.java
@@ -1,25 +1,11 @@
 package io.spring.barcelona.coffee.barista;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.TimeUnit;
-
-import javax.annotation.Nullable;
-
 import io.spring.barcelona.coffee.barista.orders.Order;
 import io.spring.barcelona.coffee.barista.orders.OrderEntry;
 import io.spring.barcelona.coffee.barista.service.KafkaHandler;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.testcontainers.containers.KafkaContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
@@ -37,8 +23,18 @@ import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Olga Maciaszek-Sharma
@@ -49,77 +45,76 @@ import org.springframework.test.context.DynamicPropertySource;
 @ActiveProfiles("contracts")
 public abstract class BaseTestClass {
 
-	@Autowired
-	KafkaHandler kafkaHandler;
+    @Autowired
+    KafkaHandler kafkaHandler;
 
-	@Container
-	@ServiceConnection
-	static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka"));
+    @Container
+    @ServiceConnection
+    static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka"));
 
-	public void triggerServing() {
-		Order order = new Order();
-		order.add(new OrderEntry("latte", 1));
-		order.add(new OrderEntry("v60", 2));
-		kafkaHandler.process(order);
-	}
+    public void triggerServing() {
+        Order order = new Order();
+        order.add(new OrderEntry("latte", 1));
+        order.add(new OrderEntry("v60", 2));
+        kafkaHandler.process(order);
+    }
 
-	public void triggerError() {
-		Order order = new Order();
-		order.add(new OrderEntry("expresso", 1));
-		kafkaHandler.process(order);
-	}
+    public void triggerError() {
+        Order order = new Order();
+        order.add(new OrderEntry("expresso", 1));
+        kafkaHandler.process(order);
+    }
 
-	@Configuration
-	@EnableKafka
-	static class TestConfig {
+    @Configuration
+    @EnableKafka
+    static class TestConfig {
 
-		@Bean
-		KafkaMessageVerifier kafkaTemplateMessageVerifier() {
-			return new KafkaMessageVerifier();
-		}
+        @Bean
+        KafkaMessageVerifier kafkaTemplateMessageVerifier() {
+            return new KafkaMessageVerifier();
+        }
 
-	}
+    }
 
-	static class KafkaMessageVerifier implements MessageVerifierReceiver<Message<?>> {
+    static class KafkaMessageVerifier implements MessageVerifierReceiver<Message<?>> {
 
-		private static final Log LOG = LogFactory.getLog(KafkaMessageVerifier.class);
+        private static final Log LOG = LogFactory.getLog(KafkaMessageVerifier.class);
 
-		Map<String, BlockingQueue<Message<?>>> broker = new ConcurrentHashMap<>();
-
-
-		@Override
-		public Message receive(String destination, long timeout, TimeUnit timeUnit, @Nullable YamlContract contract) {
-			broker.putIfAbsent(destination, new ArrayBlockingQueue<>(1));
-			BlockingQueue<Message<?>> messageQueue = broker.get(destination);
-			Message<?> message;
-			try {
-				message = messageQueue.poll(timeout, timeUnit);
-			}
-			catch (InterruptedException e) {
-				throw new RuntimeException(e);
-			}
-			if (message != null) {
-				LOG.info("Removed a message from a topic [" + destination + "]");
-				LOG.info(message.getPayload().toString());
-			}
-			return message;
-		}
+        Map<String, BlockingQueue<Message<?>>> broker = new ConcurrentHashMap<>();
 
 
-		@KafkaListener(id = "baristaContractTestListener", topics = {"errors", "servings"})
-		public void listen(ConsumerRecord payload, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
-			LOG.info("Got a message from a topic [" + topic + "]");
-			Map<String, Object> headers = new HashMap<>();
-			new DefaultKafkaHeaderMapper().toHeaders(payload.headers(), headers);
-			broker.putIfAbsent(topic, new ArrayBlockingQueue<>(1));
-			BlockingQueue<Message<?>> messageQueue = broker.get(topic);
-			messageQueue.add(MessageBuilder.createMessage(payload.value(), new MessageHeaders(headers)));
-		}
+        @Override
+        public Message receive(String destination, long timeout, TimeUnit timeUnit, @Nullable YamlContract contract) {
+            broker.putIfAbsent(destination, new ArrayBlockingQueue<>(1));
+            BlockingQueue<Message<?>> messageQueue = broker.get(destination);
+            Message<?> message;
+            try {
+                message = messageQueue.poll(timeout, timeUnit);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            if (message != null) {
+                LOG.info("Removed a message from a topic [" + destination + "]");
+                LOG.info(message.getPayload().toString());
+            }
+            return message;
+        }
 
-		@Override
-		public Message receive(String destination, YamlContract contract) {
-			return receive(destination, 15, TimeUnit.SECONDS, contract);
-		}
 
-	}
+        @KafkaListener(id = "baristaContractTestListener", topics = {"errors", "servings"})
+        public void listen(ConsumerRecord payload, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+            LOG.info("Got a message from a topic [" + topic + "]");
+            Map<String, Object> headers = new HashMap<>();
+            new DefaultKafkaHeaderMapper().toHeaders(payload.headers(), headers);
+            broker.putIfAbsent(topic, new ArrayBlockingQueue<>(1));
+            BlockingQueue<Message<?>> messageQueue = broker.get(topic);
+            messageQueue.add(MessageBuilder.createMessage(payload.value(), new MessageHeaders(headers)));
+        }
+
+        @Override
+        public Message receive(String destination, YamlContract contract) {
+            return receive(destination, 15, TimeUnit.SECONDS, contract);
+        }
+
+    }
 }
